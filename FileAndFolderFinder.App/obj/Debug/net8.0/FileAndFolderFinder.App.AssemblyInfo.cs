0f6585aed2e3c073df6c62d8a719bf7463fb97ce
Smaga Max <smaga.max@gmail.com>
2025-05-22 20:48:45 +0300

    add tests when: path contains special characters, directory not exist, keywordisEmpty

--- FileAndFolderFinder.App/obj/Debug/net8.0/FileAndFolderFinder.App.AssemblyInfo.cs
+++ FileAndFolderFinder.App/obj/Debug/net8.0/FileAndFolderFinder.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileAndFolderFinder.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4913ee3be27338be1a959a0c8355fa997bc5f28d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834f6fe62b0ab6e7609c844409078555fe0d8645")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileAndFolderFinder.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileAndFolderFinder.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FileAndFolderFinder.Tests/FileSearcherUnitTest.cs
+++ FileAndFolderFinder.Tests/FileSearcherUnitTest.cs
@@ -4,6 +4,8 @@ namespace FileAndFolderFinder.Tests
 {
     public class FileSearcherUnitTest
     {
+        private readonly FileSearcher _searcher = new FileSearcher();
+
         [Fact]
         public void Search_ShouldFindFilesOrFoldersContainingKeywords()
         {
@@ -18,19 +20,58 @@ namespace FileAndFolderFinder.Tests
                 string testFile = Path.Combine(tempDirection, "test_file.txt");
                 File.WriteAllText(testFile, "test");
 
-                FileSearcher searcher = new FileSearcher();
-
-                var results = searcher.Search(tempDirection, "test");
+                var results = _searcher.Search(tempDirection, "test");
 
                 Assert.Single(results.Directories);
                 Assert.Single(results.Files);
-                Assert.Contains(results.Directories, path => path.Contains("testFolder"));
-                Assert.Contains(results.Files, path => path.Contains("test_file.txt"));
+                Assert.Contains(testDirection, results.Directories);
+                Assert.Contains(testFile, results.Files);
             }
 
             finally
             {
                 Directory.Delete(tempDirection, true);
+            }
+        }
+
+        [Fact]
+        public void Search_ShouldThrowException_WhenDirectoryDoesNotExist()
+        {
+            var invalidDir = "invalid path";
+
+            Assert.Throws<DirectoryNotFoundException>(() =>
+            {
+                _searcher.Search(invalidDir, "keyword");
+            });
+        }
+
+        [Fact]
+        public void Search_ShouldThrowException_WhenPathContainsSpecialCharacters()
+        {
+            var invalidDir = "invalid path*";
+
+            Assert.Throws<IOException>(() =>
+            {
+                _searcher.Search(invalidDir, "keyword");
+            });
+        }
+
+        [Fact]
+        public void Search_ShouldReturnEmpty_WhenKeywordIsEmpty()
+        {
+            var tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempDir);
+
+            try
+            {
+                var results = _searcher.Search(tempDir, "");
+
+                Assert.Empty(results.Files);
+                Assert.Empty(results.Directories);
+            }
+            finally
+            {
+                Directory.Delete(tempDir, true);
             }
         }
     }
